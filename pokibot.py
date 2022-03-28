02fccfb007c9f4bfd395b301c3c9bcc4a7813b72
Onyaxoxo <onyaxoxo@gmail.com>
2022-03-28 15:44:37 +1000

    commit on master

---
+++ pokibot.py
@@ -0,0 +1,25 @@
+# 5179128627:AAE6mKC2X5oehXds5Qx1QoH1lYWut7Lx9PI
+from ast import Delete
+from posixpath import split
+import string
+from aiogram.utils import executor
+from setuptools import Command
+from create_bot import dp
+from data_base import sqlite_db
+
+
+
+
+async def on_startup(_):
+    print("Бот Онлайн") 
+    sqlite_db.sql_start()
+
+from handlers import client, admin, other
+
+client.registr_handlers_client(dp)
+admin.register_handlers_admin(dp)
+other.registr_handlers_other(dp)
+
+    
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup) 
